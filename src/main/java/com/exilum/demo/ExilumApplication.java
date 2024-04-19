2ebb2fbabfa2df9c3696279ac492408c6816cc0a
amunlozb <amunlozb@g.educaand.es>
2024-04-19 21:36:54 +0200

    spring init

---
+++ src/main/java/com/exilum/demo/ExilumApplication.java
@@ -0,0 +1,12 @@
+package com.exilum.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExilumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExilumApplication.class, args);
+	}
+}
