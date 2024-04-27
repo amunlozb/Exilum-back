4594ecae4008f4a3fa889b75479f77e8a0f40fdb
amunlozb <amunlozb@g.educaand.es>
2024-04-27 15:22:19 +0200

    added simple testing AdminController

--- src/main/java/com/exilum/demo/admin/AdminController.java
+++ src/main/java/com/exilum/demo/admin/AdminController.java
@@ -1,4 +1,27 @@
 package com.exilum.demo.admin;
 
+import com.exilum.demo.security.Permission;
+import com.google.firebase.auth.FirebaseAuthException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final UserManagementService userManagementService;
+
+    // Only allow users with ROLE_ANONYMOUS (all users have it by default)
+    @Secured("ROLE_ANONYMOUS")
+    @PostMapping(path = "/user-claims/{uid}")
+    public void setUserClaims(
+            @PathVariable String uid,
+            @RequestBody List<Permission> requestedClaims
+    ) throws FirebaseAuthException {
+        userManagementService.setUserClaims(uid, requestedClaims);
+    }
 }
--- src/main/java/com/exilum/demo/config/WebSecurityConfig.java
+++ src/main/java/com/exilum/demo/config/WebSecurityConfig.java
@@ -3,22 +3,19 @@ package com.exilum.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                // Require authentication for any request
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated()
-                )
-                // Configure oauth2 resource server
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        http.csrf().disable();
+        // Configure oauth2 resource server
+        http.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
 }
