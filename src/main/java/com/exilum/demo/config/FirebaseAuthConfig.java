e7c0d186a25e831ec61771e5799f6c0c9743fa1c
amunlozb <amunlozb@g.educaand.es>
2024-04-27 15:06:06 +0200

    added FirebaseAuthConfig

--- src/main/java/com/exilum/demo/config/FirebaseAuthConfig.java
+++ src/main/java/com/exilum/demo/config/FirebaseAuthConfig.java
@@ -4,7 +4,6 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import lombok.var;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
