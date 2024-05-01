e720bda58913dedcb0593a4ad5c701c1b2600917
amunlozb <amunlozb@g.educaand.es>
2024-05-01 11:18:33 +0200

    fixed SignUp (again)

--- src/main/java/com/exilum/demo/controllers/AuthController.java
+++ src/main/java/com/exilum/demo/controllers/AuthController.java
@@ -4,6 +4,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.exilum.demo.model.auth.SignupRequest;
@@ -15,13 +16,20 @@ public class AuthController {
     private FirebaseAuth firebaseAuth;
 
     @PostMapping("/signup")
-    public UserRecord createUser(@RequestBody SignupRequest request) throws FirebaseAuthException {
-        UserRecord.CreateRequest createRequest = new UserRecord.CreateRequest()
-                .setEmail(request.getEmail())
-                .setPassword(request.getPassword());
-        System.out.println("EMAIL: " + request.getEmail() + ",  PASSWORD: " + request.getPassword());
+    public ResponseEntity<?> createUser(@RequestBody SignupRequest request) {
+        try {
+            UserRecord.CreateRequest createRequest = new UserRecord.CreateRequest()
+                    .setEmail(request.getEmail())
+                    .setPassword(request.getPassword());
 
-        return FirebaseAuth.getInstance().createUser(createRequest);
+            UserRecord userRecord = FirebaseAuth.getInstance().createUser(createRequest);
+            System.out.println("User created successfully: " + userRecord.getUid());
+
+            return ResponseEntity.ok("User created successfully");
+        } catch (FirebaseAuthException e) {
+            System.err.println("Error creating user: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 
     @GetMapping("test")
