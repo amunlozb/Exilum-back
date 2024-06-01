98eae83aa09115eccb796325d33ce08e4920965e
amunlozb <amunlozb@g.educaand.es>
2024-06-01 12:29:43 +0200

    switched SERVICE-ACCOUNT firebase sdk to env var

--- src/main/java/com/exilum/demo/ExilumApplication.java
+++ src/main/java/com/exilum/demo/ExilumApplication.java
@@ -3,10 +3,26 @@ package com.exilum.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+
 @SpringBootApplication
 public class ExilumApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExilumApplication.class, args);
+	private static final Logger logger = LoggerFactory.getLogger(ExilumApplication.class);
+
+	public static void main(final String[] args) throws Exception {
+		String port = System.getenv("PORT");
+		if (port == null) {
+			port = "8080";
+			logger.warn("$PORT environment variable not set, defaulting to 8080");
+		}
+		SpringApplication app = new SpringApplication(ExilumApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+
+		app.run(args);
+        logger.info("Container started succesfully! Listening to HTTP requests on {}", port);
 	}
 }
--- src/test/java/com/exilum/demo/ExilumApplicationTests.java
+++ src/test/java/com/exilum/demo/ExilumApplicationTests.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -16,28 +14,4 @@ class ExilumApplicationTests {
 
     @Autowired
     private ScarabRepository scarabRepository;
-
-    @Test
-    void contextLoads() {
-    }
-
-    @Test
-    void testDatabaseConnection() {
-        Scarab testScarab = new Scarab(22L, "Scarab of the Generals", "Legion", 3, 19.99, "https://test.com");
-        scarabRepository.save(testScarab);
-
-        // Perform a simple database query
-        List<Scarab> scarabs = scarabRepository.findAll();
-
-        // Log the scarabs to verify they are being saved
-        System.out.println("Scarabs in the database: " + scarabs);
-
-        // Assert that the fetched list is not null or empty
-        assertNotNull(scarabs);
-        assertFalse(scarabs.isEmpty());
-
-        // Assert that the testScarab is in the list
-        boolean found = scarabs.stream().anyMatch(s -> s.getName().equals("Scarab of the Generals"));
-        assertTrue(found, "Test scarab should be found in the database");
-    }
 }
